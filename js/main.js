82294bb33e29a1a7ae2687e555fe1d148434d34e
Vadim Tumanov <tumanoff.vadim@yandex.ru>
2018-10-27 14:47:06 +0300

    Add project

---
+++ js/main.js
@@ -0,0 +1,193 @@
+//Модальное окно - гамбургер через JS
+
+// $(function () {
+
+//   let button = document.getElementById('burger-menu');
+//   let tabletsMenu = document.getElementById('tablets-menu');
+//   let body = document.body;
+
+//   let buttonMenuFirst = document.getElementById('menu-list__item-first');
+//   let buttonMenuSecond = document.getElementById('menu-list__item-second');
+//   let buttonMenuThird = document.getElementById('menu-list__item-third');
+
+
+//   var isOdd = true;
+
+//   button.addEventListener('click', function () {
+
+//     button.classList.toggle('burger-menu_active');
+//     tabletsMenu.classList.toggle('tablets-menu_active');
+
+//     if (isOdd) {
+//       body.setAttribute("style", "overflow: hidden")
+//       isOdd = false;
+//     } else {
+//       body.removeAttribute("style")
+//       isOdd = true;
+//     }
+
+//   })
+
+// })
+
+//Модальное окно - гамбургер через плагин Fancybox
+
+//fancybox
+
+$(function(){
+  $('[data-fancybox]').fancybox({
+    touch: false,
+    padding: 0
+  })
+})
+
+
+//Вертикальный аккордеон "Команда"
+$(function(){
+  $('.team-acco__trigger').on('click', e =>{
+    e.preventDefault();
+    const $this = $(e.currentTarget);
+    const item = $this.closest('.team-acco__item')
+    const items = $this.closest('.team-acco')
+    const container = items.find('.team-acco__item')
+
+    if(!item.hasClass('team-acco__item_active')){
+      container.removeClass('team-acco__item_active')
+      item.addClass('team-acco__item_active')
+    }else{
+      item.removeClass('team-acco__item_active')
+    }
+    
+  })
+
+})
+
+
+// Горизонтальный аккордеон "Меню"
+
+$(function(){
+  $('.menu-list__trigger').on('click', e =>{
+    e.preventDefault();
+    
+    const $this = $(e.currentTarget);
+    const item = $this.closest('.menu-list__item')
+    const container = item.closest('.menu-list')
+    const items = container.find('.menu-list__item')
+    const content = item.find('.menu__container')
+    const textBlock = content.find('.menu__container-desc')
+    
+    if(!item.hasClass('menu-list__item-active')){
+      items.removeClass('menu-list__item-active');
+      item.addClass('menu-list__item-active')
+    }else{
+      item.removeClass('menu-list__item-active')
+    }
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+//One Page Scroll 
+
+$(function(){
+
+  const display = $('.maincontent');
+  const sections = $('.section');
+
+  let inScroll = false; //убираем баг при быстрой прокрутке
+
+  //Событие на fixed menu 
+  const switchMenuActiveClass = sectionEq =>{
+    $('.menu-fixed__item').eq(sectionEq).addClass('menu-fixed__item_active')
+      .siblings().removeClass('menu-fixed__item_active')
+  }
+
+  //Функция для смены секции
+  const performTransition = sectionEq =>{
+    
+  
+    if(inScroll == true) return 
+    inScroll = true;
+    const position = (sectionEq * -100) + '%'; //определяем позицию секции
+
+
+    display.css({
+          'transform': `translate(0, ${position})`,
+          'webkit-transform' :  `translate(0, ${position})`
+        })
+
+    sections.eq(sectionEq).addClass('active')
+      .siblings().removeClass('active');
+
+    setTimeout(()=>{ //убираем баг при быстрой прокрутке
+      inScroll = false;
+      switchMenuActiveClass(sectionEq)
+    }, 1300)
+  }
+
+  //Выноис в функцию часто используемые переменные
+  const difineSections = sections =>{
+    const activeSection = sections.filter('.active')
+    return{
+      activeSection: activeSection,
+      nextSection: activeSection.next(),
+      prevSection: activeSection.prev()
+    }
+  }
+
+ //Управление колесом/тачпадом
+  $('.wrapper').on('wheel', e =>{
+    const deltaY = e.originalEvent.deltaY;
+    const section = difineSections(sections);
+    if(deltaY > 0 && section.nextSection.length){ //скролим вниз
+
+      performTransition(section.nextSection.index())
+    }
+    
+    if(deltaY < 0 && section.prevSection.length){ //скролим вверх
+      performTransition(section.prevSection.index())
+    }
+    
+  })
+
+  //Управление с клавиатуры
+  $(document).on('keydown', e=>{
+    const section = difineSections(sections);
+
+
+    if (inScroll) return
+
+    switch(e.keyCode){
+      case 40: //вверх
+      if(!section.nextSection.length)return; //если секции есть, то код ниже выполнится
+      performTransition(section.nextSection.index());
+      break;
+
+    case 38: //вниз
+      if(!section.prevSection.length)return;
+      performTransition(section.prevSection.index());
+      break;
+
+    }
+  })
+
+  //Навигация по кнопкам
+  $('[data-scroll-to').on('click', e =>{
+    e.preventDefault();
+    const $this = $(e.currentTarget);
+    //парсим строковое значение в числовое, так как  $this.attr('data-scroll-to' возвращает string
+    const sectionIndex = parseInt($this.attr('data-scroll-to'))
+   
+    performTransition(sectionIndex)
+  })
+
+})
